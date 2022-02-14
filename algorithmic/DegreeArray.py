f323bbf0aa9d2fbfb8070dd0690542784e99c11e
Fede22dev <79111932+Fede22dev@users.noreply.github.com>
2022-02-14 18:14:13 +0100

    Initial commit with some problems already solved

---
+++ algorithmic/DegreeArray.py
@@ -0,0 +1,36 @@
+import time
+
+f = open("C:/Users/Fede/Downloads/rosalind_deg.txt", "r")
+# lista di tutti i collegamenti
+listEdge = []
+for line in f:
+    edge = line.split()
+    listEdge.append(edge)
+
+# lista di tutti i vertici ma duplicati
+listNode = []
+for nodes in listEdge:
+    for node in nodes:
+        listNode.append(int(node))
+
+# lista di tutti i vertici
+listVertex = list(set(listNode))
+listVertex.sort()
+
+# lista di tot zeri quanti sono i vertici
+listDegreeVertex = [0] * len(listVertex)
+for edge in listEdge:
+    first = listVertex.index(int(edge[0]))
+    second = listVertex.index(int(edge[1]))
+
+    listDegreeVertex[first] += 1
+    listDegreeVertex[second] += 1
+
+    print(listDegreeVertex)
+    time.sleep(0.75)
+
+result = ""
+for degree in listDegreeVertex:
+    result += str(degree) + " "
+
+print(result)
---
+++ algorithmic/Fibonacci.py
@@ -0,0 +1,11 @@
+def fibonacci(num):
+    if num == 0:
+        return 0
+    elif num == 1:
+        return 1
+    else:
+        return fibonacci(num - 1) + fibonacci(num - 2)
+
+
+numS = 22
+print(fibonacci(numS))
